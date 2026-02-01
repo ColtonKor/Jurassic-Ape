e1475c593f4fc2676d679a5193b8228223b253bc
ColtonKor <ckorhummel@csumb.edu>
2026-02-01 09:35:42 -0800

    UI and Robot Animations.
    I have seen that the UI always looks at the camera in the game I was referencing. I added that and also added the basic animations for the robot. Idle and Death animations for this character.

---
+++ Assets/Scripts/Enemies/UILookAt.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class UILookAt : MonoBehaviour
+{
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward, cam.transform.rotation * Vector3.up);
+    }
+}
