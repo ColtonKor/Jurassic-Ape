33b7afbe6b9ae117ca37f336d8d28524239782f1
ColtonKor <ckorhummel@csumb.edu>
2026-02-15 12:37:12 -0800

    Added Variables to the Trials
    I added a list of SubTrials and I also added a bool that determines if the mission is completed or not.

--- Assets/Scripts/Trials.cs
+++ Assets/Scripts/Trials.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Trials", menuName = "ScriptableObjects/Trials")]
@@ -6,4 +7,8 @@ public class Trials : ScriptableObject
     public string title;
     
     public string description;
+    
+    public List<Trials> subTrials = new List<Trials>();
+
+    public bool isCompleted;
 }
