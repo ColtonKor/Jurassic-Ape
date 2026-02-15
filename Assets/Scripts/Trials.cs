45e3680a3e303c890bbd7af62302154679d96b40
ColtonKor <ckorhummel@csumb.edu>
2026-02-15 11:34:45 -0800

    I added the Trials.
    I made a base level scriptable object for the Trials I will go more in depth for these trials when the time comes.

---
+++ Assets/Scripts/Trials.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Trials", menuName = "ScriptableObjects/Trials")]
+public class Trials : ScriptableObject
+{
+    public string title;
+    
+    public string description;
+}
